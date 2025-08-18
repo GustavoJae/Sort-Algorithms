ea0b31adc1f766f40b5e2072fdc8a24b7199b079
GustavoJae <gustavojmy29@gmail.com>
2025-08-18 17:50:18 -0300

    Add bubble sort :smile:

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,13 +5,11 @@ def bubbleSort(list):
 
     # percorre toda a lista N-1 vezes
     for i in range(n-1):
-
         # percorre a lista de par em par, com n-1 pares
         for j in range(n-1):
             # ordena os pares
             if list[j] > list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
-
     return list
 
 print(bubbleSort([4,3,1,5,7,8]))
